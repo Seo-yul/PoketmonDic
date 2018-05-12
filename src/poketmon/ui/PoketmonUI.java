64255c4a61263a5307b22c3d96bd9d1ecbfec839
seoyulsay <seoyul1993@gmail.com>
2018-05-12 15:47:51 +0900

    init

--- src/poketmon/ui/PoketmonUI.java
+++ src/poketmon/ui/PoketmonUI.java
@@ -15,64 +15,75 @@ import poketmon.vo.Poketmon;
 
 public class PoketmonUI {
 	private PoketmonDAO dao = new PoketmonDAO();
-	private Poketmon poketmon= null;
+	private Poketmon poketmon = null;
+	private ArrayList<Poketmon> arryPoketmon = new ArrayList<>();
 	public static void main(String[] args) {
 		PoketmonUI ui = new PoketmonUI();
-		ui.crawl();
+		ui.init();
 	}
-	public void crawl() {
-		poketmon = new Poketmon();
-		String name = null;
-		Scanner sc = new Scanner(System.in);
-		name = sc.nextLine();
-		String url = "http://ko.pokemon.wikia.com/wiki/"+name;
-			Connection con = Jsoup.connect(url);
-			ArrayList<String> list = new ArrayList<>();
-			
-			Elements els = null;
-			int count = 0;
-			
-			String ko_name = null; //한국이름
-			String jp_name = null; //일본이름
-			String number = null; //번호
-			String type = null; //타입
-			String sort = null; //분류
-			String spec = null; //특성
-			String hspec = null; //숨겨진 특성
-			String exp = null; //경험치
-			String tall = null; //키
-			String weight = null; //몸무게
-			String crate = null; //포획률
-			String grate = null; //몸무게
-			
-			try {
-				Document doc = con.get();
-				els = doc.select("table tbody tr td");
-				for(Element ee : els) {
-					list.add(ee.text());
-					count++;
-					if(count==29)break;
-				}
-				
-				poketmon.setKo_name(doc.selectFirst(".name-ko strong").text());
-				poketmon.setJp_name(doc.selectFirst(".name-ja span").text());
-				poketmon.setNumber(doc.selectFirst("strong.rounded").text());
-				poketmon.setType(list.get(10));
-				poketmon.setSort(list.get(11));
-				poketmon.setSpec(list.get(12));
-				poketmon.setHspec(list.get(13));
-				poketmon.setExp(list.get(14));
-				poketmon.setTall(list.get(25));
-				poketmon.setTall(list.get(26));
-				poketmon.setWeight(list.get(26));
-				poketmon.setCrate(list.get(27));
-				poketmon.setGrate(list.get(28));
-				
-				System.out.println(poketmon.toString());
-				dao.insertPoketmon(poketmon);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
+
+	public void init() {
+		String initUrl = "http://ko.pokemon.wikia.com/wiki/%EA%B5%AD%EA%B0%80%EB%B3%84_%ED%8F%AC%EC%BC%93%EB%AA%AC_%EC%9D%B4%EB%A6%84_%EB%AA%A9%EB%A1%9D";
+		Connection con = Jsoup.connect(initUrl);
+		Elements els = null;
+		int count = 0;
+		try {
+			Document doc = con.get();
+			els = doc.select("a.mw-redirect");
+			for (Element ee : els) {
+				getWikidata((ee.text()));
+				count ++;
+				if(count==806)break;
 			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//
+		}
+		
+		for(Poketmon p: arryPoketmon) {
+			dao.insertPoketmon(p);
+		}
+	}
+
+	public void getWikidata(String name) {
+		poketmon = new Poketmon();
+		String url = "http://ko.pokemon.wikia.com/wiki/" + name;
+		Connection con = Jsoup.connect(url);
+		ArrayList<String> list = new ArrayList<>();
+		Elements els = null;
+		int count = 0;
+
+		try {
+			Document doc = con.get();
+			els = doc.select("table tbody tr td");
+			for (Element ee : els) {
+				list.add(ee.text());
+				count++;
+				if (count == 29)
+					break;
+			}
+
+			poketmon.setNo(doc.selectFirst("strong.rounded").text());
+			poketmon.setKor_name(doc.selectFirst(".name-ko strong").text());
+			poketmon.setJap_name(doc.selectFirst(".name-ja span").text());
+			poketmon.setType(list.get(10));
+			poketmon.setSort(list.get(11));
+			poketmon.setCharacter(list.get(12));
+			poketmon.setH_character(list.get(13));
+			poketmon.setE_point(list.get(14));
+			poketmon.setHeight(list.get(25));
+			poketmon.setWeight(list.get(26));
+			poketmon.setC_rate(Integer.parseInt(list.get(27)));
+			poketmon.setG_rate(list.get(28));
+			System.out.println(poketmon.toString());
+			arryPoketmon.add(poketmon);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//
+		}
 	}
 }
--- src/poketmon/vo/Poketmon.java
+++ src/poketmon/vo/Poketmon.java
@@ -1,116 +1,141 @@
 package poketmon.vo;
 
 public class Poketmon {
-	private String ko_name = null; //한국이름
-	private String jp_name = null; //일본이름
-	private String number = null; //번호
+	private String No = null; //번호
+	private String kor_name = null; //한국이름
+	private String jap_name = null; //일본이름
+	private String height = null; //키
+	private String weight = null; //몸무게
+	private int c_rate = 0; //포획률
+	private String g_rate = null; //성비율
 	private String type = null; //타입
 	private String sort = null; //분류
-	private String spec = null; //특성
-	private String hspec = null; //숨겨진 특성
-	private String exp = null; //경험치
-	private String tall = null; //키
-	private String weight = null; //몸무게
-	private String crate = null; //포획률
-	private String grate = null; //몸무게
+	private String character = null; //특성
+	private String h_character = null; //숨겨진 특성
+	private String e_point = null; //경험치
 	
 	public Poketmon() {
 		super();
 	}
-	
-	public Poketmon(String ko_name, String jp_name, String number, String type, String sort, String spec, String hspec,
-			String exp, String tall, String weight, String crate, String grate) {
+
+	public Poketmon(String no, String kor_name, String jap_name, String height, String weight, int c_rate,
+			String g_rate, String type, String sort, String character, String h_character, String e_point) {
 		super();
-		this.ko_name = ko_name;
-		this.jp_name = jp_name;
-		this.number = number;
+		No = no;
+		this.kor_name = kor_name;
+		this.jap_name = jap_name;
+		this.height = height;
+		this.weight = weight;
+		this.c_rate = c_rate;
+		this.g_rate = g_rate;
 		this.type = type;
 		this.sort = sort;
-		this.spec = spec;
-		this.hspec = hspec;
-		this.exp = exp;
-		this.tall = tall;
-		this.weight = weight;
-		this.crate = crate;
-		this.grate = grate;
+		this.character = character;
+		this.h_character = h_character;
+		this.e_point = e_point;
 	}
 
-	public String getKo_name() {
-		return ko_name;
+	public String getNo() {
+		return No;
 	}
-	public void setKo_name(String ko_name) {
-		this.ko_name = ko_name;
+
+	public void setNo(String no) {
+		No = no;
 	}
-	public String getJp_name() {
-		return jp_name;
+
+	public String getKor_name() {
+		return kor_name;
 	}
-	public void setJp_name(String jp_name) {
-		this.jp_name = jp_name;
+
+	public void setKor_name(String kor_name) {
+		this.kor_name = kor_name;
 	}
-	public String getNumber() {
-		return number;
+
+	public String getJap_name() {
+		return jap_name;
 	}
-	public void setNumber(String number) {
-		this.number = number;
+
+	public void setJap_name(String jap_name) {
+		this.jap_name = jap_name;
 	}
-	public String getType() {
-		return type;
+
+	public String getHeight() {
+		return height;
 	}
-	public void setType(String type) {
-		this.type = type;
+
+	public void setHeight(String height) {
+		this.height = height;
 	}
-	public String getSort() {
-		return sort;
-	}
-	public void setSort(String sort) {
-		this.sort = sort;
-	}
-	public String getSpec() {
-		return spec;
-	}
-	public void setSpec(String spec) {
-		this.spec = spec;
-	}
-	public String getHspec() {
-		return hspec;
-	}
-	public void setHspec(String hspec) {
-		this.hspec = hspec;
-	}
-	public String getExp() {
-		return exp;
-	}
-	public void setExp(String exp) {
-		this.exp = exp;
-	}
-	public String getTall() {
-		return tall;
-	}
-	public void setTall(String tall) {
-		this.tall = tall;
-	}
+
 	public String getWeight() {
 		return weight;
 	}
+
 	public void setWeight(String weight) {
 		this.weight = weight;
 	}
-	public String getCrate() {
-		return crate;
+
+	public int getC_rate() {
+		return c_rate;
 	}
-	public void setCrate(String crate) {
-		this.crate = crate;
+
+	public void setC_rate(int c_rate) {
+		this.c_rate = c_rate;
 	}
-	public String getGrate() {
-		return grate;
+
+	public String getG_rate() {
+		return g_rate;
 	}
-	public void setGrate(String grate) {
-		this.grate = grate;
+
+	public void setG_rate(String g_rate) {
+		this.g_rate = g_rate;
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSort() {
+		return sort;
+	}
+
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+
+	public String getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+
+	public String getH_character() {
+		return h_character;
+	}
+
+	public void setH_character(String h_character) {
+		this.h_character = h_character;
+	}
+
+	public String getE_point() {
+		return e_point;
+	}
+
+	public void setE_point(String e_point) {
+		this.e_point = e_point;
+	}
+
 	@Override
 	public String toString() {
-		return "Poketmon [ko_name=" + ko_name + ", jp_name=" + jp_name + ", number=" + number + ", type=" + type
-				+ ", sort=" + sort + ", spec=" + spec + ", hspec=" + hspec + ", exp=" + exp + ", tall=" + tall
-				+ ", weight=" + weight + ", crate=" + crate + ", grate=" + grate + "]";
+		return "Poketmon [No=" + No + ", kor_name=" + kor_name + ", jap_name=" + jap_name + ", height=" + height
+				+ ", weight=" + weight + ", c_rate=" + c_rate + ", g_rate=" + g_rate + ", type=" + type + ", sort="
+				+ sort + ", character=" + character + ", h_character=" + h_character + ", e_point=" + e_point + "]";
 	}
+	
 }
