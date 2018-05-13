c7bbc25f4a308a71a0f49afeef44ceb7554801e8
seoyulsay <seoyul1993@gmail.com>
2018-05-14 02:10:47 +0900

    사전기능

--- src/poketmon/ui/PoketmonUI.java
+++ src/poketmon/ui/PoketmonUI.java
@@ -1,7 +1,13 @@
 package poketmon.ui;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -18,10 +24,69 @@ public class PoketmonUI {
 	private ArrayList<Poketmon> arryPoketmon = new ArrayList<>();
 	public static void main(String[] args) {
 		PoketmonUI ui = new PoketmonUI();
-		ui.init();
+//		ui.init();
 //		ui.getWikidata("피카츄");
+//		ui.kanjiDic();
+		ui.jp();
 	}
+	public void jp() { 
+		Scanner sc = new Scanner(System.in);
+		String kanji = sc.nextLine();
+		String initUrl = "http://jpdic.naver.com/search.nhn?dicQuery="+kanji+"&x=0&y=0&query="+kanji+"&target=jpdic&ie=utf8&query_utf=&isOnlyViewEE=D";
+		Connection con = Jsoup.connect(initUrl);
+		Elements els = null;
+		try {
+			Document doc = con.get();
+			els = doc.select("dt.top_dt");
+			System.out.print(els.text()+ " : ");
+			System.out.println(els.next().text());
+			System.out.print(els.next().next().text()+ " : ");
+			System.out.println(els.next().next().next().text());
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//
+		}
+	}
+	
+	public void kanjiDic() { 
+		Scanner sc = new Scanner(System.in);
+		String kanji = sc.nextLine();
+		String initUrl = "https://kakijun.jp//main/u_kensaku.php?KANJI="+kanji;
+		String imgUrl = null;
+		Connection con = Jsoup.connect(initUrl);
+		Elements els = null;
+		try {
+			Document doc = con.get();
+			els = doc.select("span.mihon100");
+			System.out.println(els.text());
+			
+			imgUrl="https://kakijun.jp"+doc.select("#kjanimation").first().attr("src");
+			System.out.println(imgUrl);
+			URL url=new URL(imgUrl);
+			
+			InputStream is = url.openStream();
+		    File file = new File("images\\"+kanji+".gif");
+		    OutputStream os = new FileOutputStream(file);
+
+		    byte[] b = new byte[2048];
+		    int length =0;
+
+		    while ((length = is.read(b)) != -1) {
+		        os.write(b, 0, length);
+		    }
+
+		    is.close();
+		    os.close();
+			
+			  
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//
+		}
+	}
 	public void init() { 
 		String initUrl = "http://ko.pokemon.wikia.com/wiki/%EA%B5%AD%EA%B0%80%EB%B3%84_%ED%8F%AC%EC%BC%93%EB%AA%AC_%EC%9D%B4%EB%A6%84_%EB%AA%A9%EB%A1%9D";
 		Connection con = Jsoup.connect(initUrl);
